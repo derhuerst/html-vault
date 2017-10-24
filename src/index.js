4caefac9a51a1df230c128d6bf218e092c2de2dc
Jannis R <mail@jannisr.de>
2017-10-25 01:46:54 +0200

    encrypt using sodium-encryption

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 'use strict'
 
-// todo
+const encryption = require('sodium-encryption')
+
+const hintEl = document.getElementById('hint')
+const messageEl = document.getElementById('message')
+const submitEl = document.getElementById('submit')
+const keyEl = document.getElementById('key')
+
+const showError = (el, message) => {
+	el.classList.add('error')
+	hintEl.innerHTML = message
+	hintEl.classList.remove('hidden')
+	keyEl.classList.add('hidden')
+}
+const resetForm = () => {
+	hintEl.classList.add('hidden')
+	messageEl.classList.remove('error')
+	submitEl.classList.remove('error')
+	keyEl.classList.add('hidden')
+}
+
+submitEl.addEventListener('click', () => {
+	resetForm()
+
+	if (messageEl.value.length === 0) {
+		return showError(messageEl, 'Please enter a message.')
+	}
+	const data = Buffer.from(messageEl.value)
+
+	try {
+		const nonce = encryption.nonce()
+		const key = encryption.key()
+		const encrypted = encryption.encrypt(data, nonce, key)
+
+		console.log('nonce', nonce) // todo
+
+		keyEl.innerText = 'This is your key:\n' + key.toString('hex')
+		keyEl.classList.remove('hidden')
+
+		console.log('encrypted', encrypted) // todo
+	} catch (err) {
+		console.error(err)
+		return showError(submitEl, 'An error occured.')
+	}
+}, false)
